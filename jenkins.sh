0c1f540230e489455a27ec35c40b7da55fc46b28
Aleksei Badyaev <aleksei.badyaev@gmail.com>
2020-01-14 20:31:01 +0700

    Обеспечена сборка пакетов дистрибутива.

---
+++ jenkins.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# Run tests in Jenkins environment.
+
+pip install -e . stdeb3
+
+# Build distro images.
+python setup.py --command-packages=stdeb3.command bdist_deb
+rm ./*.tar.gz
+python setup.py bdist_wheel
+
+VERSION="$(python setup.py --version)"
+echo "${VERSION}" > VERSION
+if [[ -z "$BRANCH" ]]; then
+    BRANCH="$(git rev-parse --abbrev-ref HEAD | sed -e 's%/%-%g')"
+fi
+sed\
+ -e "s/VERSION/${VERSION}/g"\
+ -e "s/BRANCH/${BRANCH}/g"\
+  artifactory.json > build/artifactory.json
